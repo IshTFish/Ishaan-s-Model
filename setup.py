e36c8a2fd447cab4d043966601dcf8861c8e4c43
IshTFish <88210053+IshTFish@users.noreply.github.com>
2023-01-31 17:13:24 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-  name="RandomModel",
-  author="Yiqiao Yin",
-  author_email="Eagle0504@gmail.com",
+  name="IshaansModel",
+  author="Ishaan Chandra",
+  author_email="ishaanchandra6@gmail.com",
   license="MIT license",
   description="This package provides AI-driven solutions.",
   packages=["deeplearning", "Simple"]
